c16129157b0c5dd546030b508e5acd7caf6d1c23
Kaz <kaz@alleykatz.xyz>
2024-08-06 23:23:31 +0500

    added the loading state

---
+++ src/app/(routes)/tournaments/[id]/loading.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { LoadingSpinner } from "@/components/ui/spinner";
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <LoadingSpinner />
+    </div>
+  );
+};
+
+export default Loading;
