a6ecff48e33e8e650888fda847d79f217abcd289
Kaz <kaz@alleykatz.xyz>
2024-09-10 21:25:21 +0500

    loading state

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,12 +3,8 @@ import Lottie from "lottie-react";
 import animationData from "../../public/logo.json";
 export default function Custom404() {
   return (
-    <div className="h-screen w-screen">
-      <Lottie
-        animationData={animationData}
-        className="w=full h-full"
-        loop={true}
-      />
+    <div className="h-screen w-screen text-white text-8xl">
+      LOADING ...................
     </div>
   );
 }
